8236a4f14e0ebad432744abc8a6e72a1993458dc
Jose Ayala <32329020+Neuralbytesfl@users.noreply.github.com>
2024-08-17 11:37:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from windows import MainApp
-from sys import platform
+from sys import platform, argv
 
 if platform.lower() == "win32":
     import ctypes
@@ -7,4 +7,6 @@ if platform.lower() == "win32":
     ctypes.windll.shcore.SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE)
 
 if __name__ == "__main__":
-    MainApp()
+    # Check if a file is passed as an argument
+    macro_file = argv[1] if len(argv) > 1 else None
+    MainApp(macro_file=macro_file)
